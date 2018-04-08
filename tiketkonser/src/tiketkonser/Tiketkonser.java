8eb6bf5b35f14f8c13c4c135a85f69544909129f
inf18 <38174595+inf18@users.noreply.github.com>
2018-04-09 01:48:25 +0700

    Add files via upload

---
+++ tiketkonser/src/tiketkonser/Tiketkonser.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiketkonser;
+
+/**
+ *
+ * @author Nadia
+ */
+public class Tiketkonser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login tampil = new login();
+                   tampil.setVisible(true);
+    }
+}
